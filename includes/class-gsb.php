72f4bcf62531b04f6db7a183cd25a3d30bf6465e
ozh <ozh@ozh.org>
2014-03-30 22:07:10 +0200

    First commit

---
+++ includes/class-gsb.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Google Safe Browsing Lookup client for YOURLS
+ *
+ */
+
+class ozh_yourls_GSB {
+    
+    const PROTOCOL_VER = '3.0';
+    const CLIENT       = 'yourls-plugin';
+    const APP_VER      = '1.0';
+    
+    private $url       = '';
+    private $api_key   = false;
+
+    /**
+     * Constructor : checks that plugin is properly configured
+     *
+     */
+    public function __construct( $api_key ) {
+        $this->api_key = $api_key;
+    }
+
+    /**
+     * Check if a URL is blacklisted against GSB Lookup API
+     *
+     * The function returns an array of a boolean and a string.
+     * The boolean indicates whether $this->url is blacklisted (true) or not blacklisted (false)
+     * The string gives diagnosis details: reason of blacklisting, null if clear, or an error message if applicable
+     *
+     * @return array array of boolean ( is blacklisted, description ) 
+     */
+    public function is_blacklisted( $url ) {
+        if( !$this->api_key ) {
+            return false;
+        }
+    
+        $this->url = urlencode( yourls_sanitize_url( $url ) );
+        if( !$this->url ) {
+            return false;
+        }
+        
+        $request = $this->request();
+        
+        // var_dump( $request );
+        
+        switch( $request->status_code ) {
+            case 200:
+                return array( true, $request->body );
+                
+            case 204:
+                return array( false, null );
+        
+            case 400:
+                return array( false, 'Could not check Google Safe Browsing: Bad Request' );
+        
+            case 403:
+                return array( false, 'Could not check Google Safe Browsing: API key not authorized' );
+        
+            case 503:
+                return array( false, 'Could not check Google Safe Browsing: service unavailable' );
+        
+        }
+    }
+    
+    /**
+     * HTTP request wrapper
+     *
+     * @return Request request object
+     */
+    private function request() {
+        $api_url = sprintf( 'https://sb-ssl.google.com/safebrowsing/api/lookup?client=%s&apikey=%s&appver=%s&pver=%s&url=%s',
+            self::CLIENT,
+            $this->api_key,
+            self::APP_VER,
+            self::PROTOCOL_VER,
+            $this->url
+        );
+        
+        // Request options ?
+        $options = array(
+        );
+        
+        return yourls_http_get( $api_url, array(), array(), $options );
+        
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+Plugin for YOURLS 1.7+: Google Safe Browsing
+
+# What for
+
+Check every new URL against Google's Safe Browsing Lookup service, reject those who are identified as malware or phishing
+
+# How to
+
+* In `/user/plugins`, create a new folder named `google-safe-browsing`
+* Drop these files in that directory
+* Go to the Plugins administration page and activate the plugin 
+* Follow on-screen instructions
+* Have fun
+
+# Disclaimer
+
+Using this plugin requires you to understand Google's Safe Browsing TOS. In short:
+* you need a Google account
+* you are limited to a certain amount of queries per day (10,000 as of writing this)
+* you must understand that the service is not perfect.
+
+[Read more](https://developers.google.com/safe-browsing/lookup_guide#AcceptableUsage)
